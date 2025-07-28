7ad6a3c56ab118831fa715dba0aadebbb601f1c6
denisoliveiraa <denis_o@hotmail.com.br>
2025-07-27 22:14:18 -0300

    extract adjusting

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -3,17 +3,21 @@ import requests
 def extract_data():
 
   # Latitude e Longitude de curitiba
-  url = "https://api.open-meteo.com/v1/forecast?latitude=25.25&longitude=49.16&hourly=temperature_2m"
-
+  url = "https://api.open-meteo.com/v1/forecast?latitude=22.25&longitude=49.16&hourly=temperature_2m"
   headers = {"accept": "application/json"}
+  baseHourWeather = None
+  baseTemperatureWeather = None
 
-  response = requests.get(url, headers=headers)
-  data = response.json()
+  try: 
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    baseHourWeather = data["hourly"]["time"][-1]
+    baseTemperatureWeather = data["hourly"]["temperature_2m"][-1]
+    print(baseHourWeather)
+    print(baseTemperatureWeather)
+    
+  except Exception as error:
+    print(f'we have a problem: {error}')
+    
   
-  baseHourWeather = data["hourly"]["time"][-1]
-  tempeatureOnBaseHour = data["hourly"]["temperature_2m"][-1]
-
-
-  print(baseHourWeather)
-  print(tempeatureOnBaseHour)
 extract_data()
