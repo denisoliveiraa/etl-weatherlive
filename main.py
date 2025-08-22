d3d29f9867163cd15dbd26f6aaa8fae1dcda5e78
denisoliveiraa <denis_o@hotmail.com.br>
2025-08-21 23:09:38 -0300

    feature: Logic transformers data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.etl.extract import extract_data
+from src.etl.transform import transform_weather_data
 import logging
 from dotenv import load_dotenv
 import os
@@ -16,4 +17,12 @@ if __name__ == "__main__":
     base_url = os.getenv("BASE_URL")
     weather_data = extract_data(base_url)
     if weather_data:
-        logging.info(f"Dados brutos: {weather_data}")
+        try:
+            weather_result = transform_weather_data(weather_data)
+            print(weather_result)
+            logging.info(f"Temperatura: {weather_result}")
+        except:
+           logging.error("Weather data not found")
+    else: 
+        logging.error("Erro to access extract_data")
+        
--- src/etl/transform.py
+++ src/etl/transform.py
@@ -1,6 +1,21 @@
+import pandas as pd
+import logging
 
+def transform_weather_data(weather_details) : #-> dict | None
+  temperature = weather_details['properties']['temperature']['value']
+  print(temperature)
+  try:
+    temperature = weather_details['properties']['temperature']['value']
+    humidity = weather_details['properties']['relativeHumidity']['value']
+    wind_speed = weather_details['properties']['windSpeed']['value']
+    wind_dir = weather_details['properties']['windDirection']['value']
+    
+    
+    #data = pd.DataFrame[{"temperature": temperature, "humidity": humidity, "wind_speed": wind_speed, "wind_dir": wind_dir }]
+    #logging.log("Transform data success")
 
-temperature = data['properties']['temperature']['value']
-humidity = data['properties']['relativeHumidity']['value']
-wind_speed = data['properties']['windSpeed']['value']
-wind_dir = data['properties']['windDirection']['value']
+    return humidity
+  except: 
+    logging.error("Transform data Error")
+
+  
