000c710794585e0d60cb73dc5e6f50115a7b548e
denisoliveiraa <denis_o@hotmail.com.br>
2025-08-25 23:28:32 -0300

    Feature: Adding airflow and extract ETL

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from src.etl.extract import extract_data
-from src.etl.transform import transform_weather_data
+from airflow_dags.dags.weather_temperature_daily import task_extract
 import logging
 from dotenv import load_dotenv
 import os
@@ -13,16 +12,12 @@ logging.basicConfig(
 
 
 if __name__ == "__main__":
-    load_dotenv()
+    load_dotenv()    
     base_url = os.getenv("BASE_URL")
-    weather_data = extract_data(base_url)
-    if weather_data:
-        try:
-            weather_result = transform_weather_data(weather_data)
-            print(weather_result)
-            logging.info(f"Temperatura: {weather_result}")
-        except:
-           logging.error("Weather data not found")
-    else: 
-        logging.error("Erro to access extract_data")
+    try:
+        task_extract(base_url)
+        logging.info("DAG weather temperature daily ACCESS SUCCESS")
+    except:
+        logging.error("DAG weather temperature daily ERROR")
+
         
